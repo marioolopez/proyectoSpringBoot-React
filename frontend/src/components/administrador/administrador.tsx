34f1617a807ee09a5a0d4a999f78a97936b563d7
marioolopez <150073715+marioolopez@users.noreply.github.com>
2025-12-30 18:57:52 +0100

    c

--- frontend/src/components/administrador/administrador.tsx
+++ frontend/src/components/administrador/administrador.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
 import { administradorService } from "../../services/administradorService";
 import type { administrador } from "../../models/administrador";
-import {
-  Table, TableBody, TableCell, TableContainer,
-  TableHead, TableRow, Paper, Button, TextField
-} from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField } from "@mui/material";
 
 function Administrador() {
   const [admins, setAdmins] = useState<administrador[]>([]);
@@ -12,14 +9,15 @@ function Administrador() {
   const [email, setEmail] = useState("");
   const [editId, setEditId] = useState<number | null>(null);
 
-  useEffect(() => {
-    cargarAdmins();
-  }, []);
-
   const cargarAdmins = async () => {
     const data = await administradorService.getAll();
     setAdmins(data);
   };
+
+  useEffect(() => { 
+    cargarAdmins();
+  }, []);
+  
 
   const guardar = async () => {
     if (!nombre || !email) return;
@@ -52,19 +50,10 @@ function Administrador() {
       <h2>CRUD Administradores</h2>
 
       <div style={{ display: "flex", gap: 10, marginBottom: 20 }}>
-        <TextField
-          label="Nombre"
-          value={nombre}
-          onChange={(e) => setNombre(e.target.value)}
-        />
-        <TextField
-          label="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Button variant="contained" color="primary" onClick={guardar}>
-          {editId === null ? "Crear" : "Actualizar"}
-        </Button>
+        <TextField label="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        <TextField label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        
+        <Button variant="contained" color="primary" onClick={guardar}> {editId === null ? "Crear" : "Actualizar"} </Button>
       </div>
 
       <TableContainer component={Paper}>
